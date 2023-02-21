086b281020062af2c7f1f677c78a351f605decd6
Aaron Richards <aaron_richards@hotmail.co.uk>
2023-02-21 19:37:17 +0000

    update comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import RouteSwitch from "./RouteSwitch";
+import RouteSwitch from "./js/RouteSwitch";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

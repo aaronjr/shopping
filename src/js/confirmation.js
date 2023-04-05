edd395f4947a71e16890dc32fe707403bb225d2d
Aaron Richards <aaron_richards@hotmail.co.uk>
2023-04-05 15:18:46 +0100

    minor changes to media queries

--- src/js/confirmation.js
+++ src/js/confirmation.js
@@ -12,7 +12,7 @@ export const Confirmation = () => {
         <p className="confP">Your order has been succesfully placed.</p>
         <p className="confP">Paid using saved card ending in 8172.</p>
         <p className="confP">
-          Your paint will be delivered in 3-5 working days.
+          Your order will be delivered in 3-5 working days.
         </p>
       </div>
     </div>
